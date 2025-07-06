4dcf7cf3ea659605bf88802d2bf115f2a93e2c4c
RajRathinam <rajrathinam2005@gmail.com>
2025-07-06 16:20:15 +0530

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,10 +1,4 @@
 const baseUrl = "https://permiso-vanilla.onrender.com";
-<<<<<<< HEAD
+
 // const baseUrl = "http://localhost:5000";
-=======
-//const baseUrl = "http://localhost:5000";
->>>>>>> fca916b7b60c6a60de826d867a80069af25d7a63
-
-
-
 export default baseUrl;
